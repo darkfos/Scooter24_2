82b404a9f6232956aedbaa81605d90d00d442d6f
darkfos82 <darkfos82@gmail.com>
2024-05-19 16:43:54 +0300

    lifespan and redirect to docs

--- ScooterBackend/main.py
+++ ScooterBackend/main.py
@@ -2,15 +2,31 @@
 from settings import APISettings
 
 #Other
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import RedirectResponse
+from contextlib import asynccontextmanager
 import uvicorn
 import logging
+
+
+@asynccontextmanager
+async def connection_db(app: FastAPI) -> None:
+    #lifespan for db
+    yield
 
 
 #Application (API)
 app: FastAPI = FastAPI(
     title="Scooter API",
-    description="Программный интерфейс для сайта по продаже авто деталей")
+    description="Программный интерфейс для сайта по продаже авто деталей",
+    lifespan=connection_db
+)
+
+
+#Redirect to docs
+@app.get(path="/", status_code=status.HTTP_200_OK, response_class=RedirectResponse)
+async def redirect_to_docs() -> RedirectResponse:
+    return RedirectResponse("/docs")
 
 
 #Point of entry
