1e4595f5298cf4f4afa54ec70653337ecb5df596
darkfos82 <darkfos82@gmail.com>
2024-06-10 21:29:07 +0300

    Добавил докер, протестировал миграции, создание, скрипт для заполнения данных

---
+++ migrations/versions/bdd4506ecba2_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: bdd4506ecba2
+Revises: 
+Create Date: 2024-06-01 19:02:33.077911
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "bdd4506ecba2"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
