8a24eda3a9692290dd3e47c287bea18147fd7290
darkfos82 <darkfos82@gmail.com>
2024-06-12 19:34:33 +0300

    Добавил модели Вакансии и тип работников

---
+++ ScooterBackend/database/models/type_worker.py
@@ -0,0 +1,38 @@
+#System
+from typing import Dict, Union, List
+
+
+#Other libraries
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String
+
+#Local
+from ScooterBackend.database.mainbase import MainBase
+
+
+class TypeWorker(MainBase):
+
+    #Название типа работника
+    name_type: Mapped[str] = mapped_column(type_=String(300), unique=True, nullable=False, index=True)
+
+    #Relation
+    vacancies: Mapped[List["Vacancies"]] = relationship("Vacancies", uselist=True, back_populates="type_work")
+
+    def __str__(self) -> str:
+        #Возвращает строковый объект
+        return str({
+            k: v
+            for k, v in self.__dict__.items()
+            if not k.startswith("_")
+        })
+
+    def __repr__(self) -> str:
+        #Возвращает строковый объект
+        return self.__str__()
+
+    def read_model(self) -> Dict[str, Union[str, int]]:
+        #Чтение модели
+        return {
+            "id": self.id,
+            "name_type": self.name_type
+        }
