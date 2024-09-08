4223adee93d29db7464cac3048434c15f5a7ee2c
darkfos82 <darkfos82@gmail.com>
2024-09-08 14:39:41 +0300

    add: cat_model, fav_model, hist:model

---
+++ ScooterBackend/admin/models/favourite_admin_model.py
@@ -0,0 +1,33 @@
+from sqladmin import ModelView
+from database.models.favourite import Favourite
+from typing import List, Any
+
+
+class FavouriteModelView(ModelView, model=Favourite):
+
+    #Metadata
+    name: str = "Избранные товары"
+    name_plural: str = "Добавить избранное"
+    icon: str = "fa-star-half-o"
+    category: str = "Пользователь"
+
+    column_list: List[Any] = [Favourite.id, Favourite.id_user, Favourite.id_product, Favourite.fav_user, Favourite.product_info]
+    
+    #Operations
+    can_create: bool = True
+    can_delete: bool = True
+    can_edit: bool = True
+    can_export: bool = True
+    can_view_details: bool = True
+
+    #Form's for FK
+    form_ajax_refs: dict = {
+        "fav_user": {
+            "fields": {"id", "name_user"},
+            "order_by": ("id")
+        },
+        "product_info": {
+            "fields": {"id", "title_product"},
+            "order_by": {"id"}
+        }
+    }
--- ScooterBackend/admin/models/user_admin_model.py
+++ ScooterBackend/admin/models/user_admin_model.py
@@ -1,9 +1,9 @@
 from sqladmin import ModelView
 from database.models.user import User
-from typing import List
+from typing import List, Any
 
 
-class UserAdminTable(ModelView, model=User):
+class UserModelView(ModelView, model=User):
 
     #Metadata
     name: str = "Пользователь"
@@ -12,8 +12,8 @@ class UserAdminTable(ModelView, model=User):
     category: str = "Пользователь"
 
     #Columns
-    column_list: List = [
-        User.id, User.name_user, User.email_user,
+    column_list: List[Any] = [
+        User.id, User.name_user, User.email_user, User.password_user,
         User.surname_user, User.main_name_user, User.surname_user_address,
         User.address_area, User.address_index, User.address_locality,
         User.country_address, User.address_phone_number, User.address_rl_et_home,
@@ -30,5 +30,5 @@ class UserAdminTable(ModelView, model=User):
     #Form's
     form_create_rules: List = ["name_user", "email_user", "surname_user", "main_name_user", "surname_user_address",
                                "address_area", "address_index", "address_locality", "country_address", "address_phone_number",
-                               "address_rl_et_home", "name_company_address", "date_registration", "date_update"]
-    form_edit_rules = form_create_rules.copy()
+                               "address_rl_et_home", "name_company_address", "password_user", "date_update", "date_registration"]
+    form_edit_rules = form_create_rules[:-1].copy()
