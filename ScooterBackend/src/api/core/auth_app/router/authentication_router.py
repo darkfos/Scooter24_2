3d87b288810afead96198cb104e05dce60efb9ae
darkfos82 <darkfos82@gmail.com>
2025-05-30 01:26:21 +0300

    refactor

--- ScooterBackend/src/api/core/auth_app/router/authentication_router.py
+++ ScooterBackend/src/api/core/auth_app/router/authentication_router.py
@@ -104,20 +104,8 @@ async def exit_user():
 
     resp = JSONResponse(content=None, status_code=status.HTTP_204_NO_CONTENT)
 
-    resp.delete_cookie(
-        key="access_key",
-        path="/",
-        httponly=True,
-        secure=True,
-        samesite="lax"
-    )
-    resp.delete_cookie(
-        key="refresh_key",
-        path="/",
-        httponly=True,
-        secure=True,
-        samesite="none"
-    )
+    resp.delete_cookie("access_key", domain="xn--24-olct5adih.xn--p1ai", path="/", httponly=True, secure=True, samesite="lax") # noqa
+    resp.delete_cookie("refresh_key", domain="xn--24-olct5adih.xn--p1ai", path="/", httponly=True, secure=True, samesite="none") # noqa
 
     return resp
 
